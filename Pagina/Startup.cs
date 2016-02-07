d4c600164cc6466967507549c2c694e4ca6e4b72
Santiago Jara <swja1991@hotmail.com>
2016-02-07 17:18:50 -0500

    Primera subida

---
+++ Pagina/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pagina.Startup))]
+namespace Pagina
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
